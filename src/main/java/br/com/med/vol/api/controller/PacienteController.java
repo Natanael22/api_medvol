0d51de0dd8cf5eb34c68eb0291dd76ac71a71f20
Natanael22 <natanaelcorreia1@gmail.com>
2023-04-19 11:50:11 -0300

    criado classes de paciente e adicionado novas migrations

--- src/main/java/br/com/med/vol/api/controller/PacienteController.java
+++ src/main/java/br/com/med/vol/api/controller/PacienteController.java
@@ -1,7 +1,13 @@
 package br.com.med.vol.api.controller;
 
 import br.com.med.vol.api.paciente.DadosCadastroPaciente;
+import br.com.med.vol.api.paciente.Paciente;
+import br.com.med.vol.api.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
     @PostMapping
-    public void cadastrar(DadosCadastroPaciente paciente){
-        System.out.println(paciente);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente paciente){
+
+        repository.save(new Paciente(paciente));
     }
 }
