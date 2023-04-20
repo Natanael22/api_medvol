604738a4c59e5d0d0f1e49c6bf30ca557de867f2
Natanael22 <natanaelcorreia1@gmail.com>
2023-04-19 21:43:35 -0300

    feito listagem de pacientes

--- src/main/java/br/com/med/vol/api/controller/PacienteController.java
+++ src/main/java/br/com/med/vol/api/controller/PacienteController.java
@@ -1,15 +1,16 @@
 package br.com.med.vol.api.controller;
 
 import br.com.med.vol.api.paciente.DadosCadastroPaciente;
+import br.com.med.vol.api.paciente.DadosListagemPaciente;
 import br.com.med.vol.api.paciente.Paciente;
 import br.com.med.vol.api.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,5 +23,10 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente paciente){
 
         repository.save(new Paciente(paciente));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarTodos(@PageableDefault(page=0,size=10,sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
---
+++ src/main/java/br/com/med/vol/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,8 @@
+package br.com.med.vol.api.paciente;
+
+public record DadosListagemPaciente(String nome, String email, String cpf) {
+
+    public DadosListagemPaciente(Paciente paciente){
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+}
